af039e6a5336205b1d9b088c9c73b2e6a6a2e348
Oto Sales <ottosjr@hotmail.com>
2024-10-28 14:24:56 -0300

    alterando arquivos

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header className="Header">
+        <h1>Header</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,19 @@
+import "./style.css"
+
+function NavBar () {
+    return (
+    <div className="Navbar">
+        <h1>Loja Sem Nome</h1>
+
+        <nav>
+            <ul>
+                <li><a href="https://google.com">Home</a></li>
+                <li><a href="https://google.com">Produtos</a></li>
+                <li><a href="https://google.com">Novidades</a></li>
+            </ul>
+        </nav>
+    </div>
+    );
+}
+
+export default NavBar;
